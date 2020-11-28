6ccfe419906e07328339c594eaa7286271c5ad30
Akhil <akhilaand@gmail.com>
2020-11-28 18:38:19 +0530

    completed

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Bottom_Navigation());
+    return MaterialApp(
+        debugShowCheckedModeBanner: false, home: Bottom_Navigation());
   }
 }
